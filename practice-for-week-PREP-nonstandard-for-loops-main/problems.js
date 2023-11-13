c779ca44e64de58b9acfb572cc6371de93111872
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2023-11-13 17:10:11 +0000

    exercise done on non-standard loop

--- practice-for-week-PREP-nonstandard-for-loops-main/problems.js
+++ practice-for-week-PREP-nonstandard-for-loops-main/problems.js
@@ -1,33 +1,33 @@
 function oddIndices(arr) {
     // Return an array containing all the odd indices in the array
-    // Your code here
+    return arr.filter((el, i) => i % 2 !== 0);
 }
 
 function oddReverse(arr) {
     // Return an array containing all the odd indices starting from the end
-    // Your code here
+    return arr.filter((el, i) => i % 2 !== 0).reverse();
 }
 
 function secondPower(arr) {
     // Return an array containing all indices that are powers of 2
-    // Your code here
+    return arr.filter((el, i) => Math.log2(i) % 1 === 0);
 }
 
 function nthPower(arr, n) {
     // Return an array containing all indices that are powers of n
-    // Your code here
+    return arr.filter((el, i) => Math.log(i) / Math.log(n) % 1 === 0);
 }
 
 function firstHalf(arr) {
     // Return an array containing the first half of an array
     // Include middle index on odd length arr
-    // Your code here
+    return arr.slice(0, Math.ceil(arr.length / 2));
 }
 
 function secondHalf(arr) {
     // Return an array containing the second half of an array
     // Exclude middle index on odd length arr
-    // Your code here
+    return arr.slice(Math.ceil(arr.length / 2));
 }
 
 module.exports = {
