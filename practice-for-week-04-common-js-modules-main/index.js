2aadbb7588777522d5cb345ece5dde3056f13575
Achara Chisom Solomon <ca0102c@gre.ac.uk>
2023-11-17 14:05:04 +0000

    all modules exported and imported correctly

--- practice-for-week-04-common-js-modules-main/index.js
+++ practice-for-week-04-common-js-modules-main/index.js
@@ -1,5 +1,7 @@
-
-// Your code here
+let { message1, message2, message3 } = require("./messages");
+const sayHelloTo = require("./send-messages/say-hello-to");
+const giveMessageToMrsPotato = require("./send-messages/give-message-to-mrs-potato");
+[msg1, msg2, msg3] = [message1, message2, message3];
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
